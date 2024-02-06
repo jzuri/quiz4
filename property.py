7eda24e04fbd63e24c5cb51d3bff6c4c59f760e8
jzuri <123331421+jzuri@users.noreply.github.com>
2024-02-05 21:29:23 -0600

    Update property.py

--- property.py
+++ property.py
@@ -1,1 +1,21 @@
+class Square:
+    def __init__(self, length):
+        self._length = length
+
+    @property
+    def length(self):
+        return self._length
+
+    @property
+    def width(self):
+        return 2 * self._width
+
+    @property
+    def area(self):
+        return self._width * self._length
+
+# Example usage:
+if __name__ == "__main__":
+    # Creating Square class
+    my_square = Square(length=5)
 
