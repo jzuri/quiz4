eb0cadd86dd25858406ba73a712d5b35d16f0229
jzuri <123331421+jzuri@users.noreply.github.com>
2024-02-05 19:55:03 -0600

    Update abc_class.py

--- abc_class.py
+++ abc_class.py
@@ -9,3 +9,25 @@ class FamousSongs(ABC):
     def song_title(self):
         pass
 
+class Country(FamousSongs):
+    def artists(self):
+        print("Implementation of artists in Country")
+
+    def song_title(self):
+        print("Implementation of song_title in Country")
+
+class Pop(FamousSongs):
+    def artists(self):
+        print("Implementation of artists in Pop")
+
+    def song_title(self):
+        print("Implementation of song_title in Pop")
+
+# Example usage:
+obj1 = Country()
+obj1.artists()
+obj1.song_title()
+
+obj2 = Pop()
+obj2.artists()
+obj2.song_title()
