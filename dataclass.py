c2c661d2a19b9a01544f8da4b76f7b015d90722c
jzuri <123331421+jzuri@users.noreply.github.com>
2024-02-05 21:04:51 -0600

    Update dataclass.py

--- dataclass.py
+++ dataclass.py
@@ -1,1 +1,25 @@
+from dataclasses import dataclass
+
+@dataclass
+class Animal:
+    name: str
+    age: int
+    type: str
+
+    def greet(self) -> str:
+        return f"This is {self.name}. They are {self.age} years old, and is a {self.type}."
+
+# Example usage:
+if __name__ == "__main__":
+    # Creating instances of the Person class using the automatically generated __init__ method
+    pet1 = Animal(name="Aly", age=2, type="Hamster")
+    pet2 = Animal(name="Billy", age=3, type="Gecko")
+
+    # Accessing attributes
+    print(pet1.name)
+    print(pet2.age)
+
+    # Calling a method
+    greeting = pet1.greet()
+    print(greeting)
 
