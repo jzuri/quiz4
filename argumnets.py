1f5bd630ac9a3faa2664860fa8da8ef43e3dd15c
jzuri <123331421+jzuri@users.noreply.github.com>
2024-02-05 18:10:06 -0600

    Update argumnets.py

--- argumnets.py
+++ argumnets.py
@@ -1,1 +1,18 @@
+import argparse
 
+def main():
+  parser = argparse.ArgumentParser(description = 'Parse string, int, and float arguments.')
+  
+  parser.add_argument('input_string', type = str, help = 'Input String')
+  parser.add_argument('input_string', type = int, help = 'Input Integer')
+  parser.add_argument('input_float', type = float, help = 'Input Float')
+
+  args = parser.parse_args()
+
+  print('String:', args.input_string)
+  print('Integer:', args.input_int)
+  print('Float:', args.input_float)
+
+if __name__ = "__main__":
+  main()
+    
