05a3a577a6f19911e1cedaee503603d1b5790624
jzuri <123331421+jzuri@users.noreply.github.com>
2024-02-05 19:33:32 -0600

    Update abc_class.py

--- abc_class.py
+++ abc_class.py
@@ -1,1 +1,11 @@
+from abc import ABC, abstractmethod
+
+class FamousSongs(ABC):
+    @abstractmethod
+    def artists(self):
+        pass
+
+    @abstractmethod
+    def song_title(self):
+        pass
 
