8c18f0b8046435a26fae165d5564dbcea082ac51
jzuri <123331421+jzuri@users.noreply.github.com>
2024-02-05 21:41:05 -0600

    Update run.py

--- Question8/run.py
+++ Question8/run.py
@@ -1,1 +1,18 @@
+# run.py
+
+from folder1.module1 import function1
+from folder2.module2 import function2
+from folder3.module3 import function3
+
+def main():
+    result1 = function1()
+    result2 = function2()
+    result3 = function3()
+
+    print(result1)
+    print(result2)
+    print(result3)
+
+if __name__ == "__main__":
+    main()
 
