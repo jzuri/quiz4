df84a676177566d51179a4c072e6435884d3694b
jzuri <123331421+jzuri@users.noreply.github.com>
2024-02-05 21:32:43 -0600

    Update no_property.py

--- no_property.py
+++ no_property.py
@@ -1,1 +1,19 @@
+class Square:
+    def __init__(self, length):
+        self._length = length
+
+    def get_length(self):
+        return self._length
+
+    def get_width(self):
+        return 2 * self._width
+
+    def get_area(self):
+        return self._width * self._length
+
+# Example usage:
+if __name__ == "__main__":
+    # Square class 2.0
+    my_square = Square(length=5)
+
 
